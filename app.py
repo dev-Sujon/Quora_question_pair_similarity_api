199609f17b3778b23d92fcf22d808608268f0bbe
dev-Sujon <120054944+dev-Sujon@users.noreply.github.com>
2024-03-29 01:03:38 +0600

    Add files via upload
    app.py file and requirements.txt are uploaded

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import streamlit as st
+import numpy as np 
+import pandas as pd 
 from sentence_transformers import SentenceTransformer, util
+import os
 
 # Load the SentenceTransformer model
 model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
